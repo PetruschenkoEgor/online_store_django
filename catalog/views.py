a67ea248ebf369da59f5c9659d21d0221129b1e7
Egor Petruschenko <jaguar.94@mail.ru>
2024-11-03 22:33:41 +0600

    Кастомная команда для добавления данных из фикстуры

--- catalog/views.py
+++ catalog/views.py
@@ -4,16 +4,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # Получение данных из формы
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
         # Обработка данных (например, сохранение в БД, отправка email и т. д.)
         print(f"{name}({email}): {message}")
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено!")
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
