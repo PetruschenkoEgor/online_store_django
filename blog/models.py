aaa78a952898cb6a7a0736fcc5f35c18504cb9e5
Egor Petruschenko <jaguar.94@mail.ru>
2024-11-19 01:12:46 +0600

    создал модель для статьи, применил миграции

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(
+        max_length=250,
+        verbose_name='Заголовок',
+        help_text='Введите название записи',
+    )
+    content = models.TextField(
+        verbose_name='Текст поста',
+        help_text='Введите текст поста',
+    )
+    image = models.ImageField(
+        upload_to='blog/photo',
+        verbose_name='Фото',
+        help_text='Загрузите фото для поста',
+        blank=True,
+        null=True,
+    )
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+    sign_of_publication = models.BooleanField(verbose_name='Признак публикации')
+    views_counter = models.PositiveIntegerField(
+        verbose_name='Счетчик просмотров',
+        help_text='Введите количество просмотров',
+        default=0,
+    )
--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,4 @@ from blog.apps import BlogConfig
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('',)
 ]
