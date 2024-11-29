9b30e2322dba6d8bd647b4b6ebfa1eb53ea84807
Egor Petruschenko <jaguar.94@mail.ru>
2024-11-29 11:59:46 +0600

    валидатор для проверки формата и размера изображения

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,3 +1,5 @@
+import os
+
 from django import forms
 from django.forms import BooleanField
 
@@ -22,10 +24,27 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
 
     def clean_price(self):
-        """ Валидатор цены """
+        """ Валидатор цены(не отрицательная) """
         price = self.cleaned_data.get('price')
         if price < 0:
             self.add_error('price', 'Цена не может быть отрицательной!')
+        return price
+
+    def clean_image(self):
+        """ Валидатор изображения(только JPEG, PNG и не больше 5 МБ) """
+        image = self.cleaned_data.get('image')
+
+        # Проверяем формат изображения
+        extension = ['jpg', 'png']
+        file = os.path.splitext(image.name)[1].lower()[1:]
+        if file not in extension:
+            self.add_error('image', 'Формат изображения может быть только JPG или PNG!')
+
+        # Проверяем, что размер изображения не превышает максимальный
+        max_size = 5 * 1024 * 1024
+        if image.size > max_size:
+            self.add_error('image', f'Размер файла не должен превышать 5 МБ!')
+        return image
 
     def clean(self):
         """ Добавлен список слов-исключений для названия и описания """
@@ -38,5 +57,5 @@ class ProductForm(forms.ModelForm):
         for word in words:
             if word in name.lower():
                 self.add_error('name', f'Название не может содержать слово {word}!')
-            elif word in description.lower():
+            if word in description.lower():
                 self.add_error('description', f'Описание не может содержать слово {word}!')
